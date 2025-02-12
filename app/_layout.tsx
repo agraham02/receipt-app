cd9339ed641bb5180db93b6af0814a2b24492904
Ahmad Graham <ahmadgrahamdev@gmail.com>
2025-02-12 17:57:23 -0500

    Wrap RootLayout in ImageProvider for context management

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,18 @@
+// _layout.tsx
+import { ImageProvider } from "@/context/ImageContext";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
     return (
-        <Stack>
-            <Stack.Screen name="index" options={{ title: "Home" }} />
-            <Stack.Screen
-                name="camera"
-                options={{ title: "Processed Text" }}
-            />
-            <Stack.Screen
-                name="processed-text"
-                options={{ title: "Processed Text" }}
-            />
-        </Stack>
+        <ImageProvider>
+            <Stack>
+                <Stack.Screen name="index" options={{ title: "Home" }} />
+                {/* <Stack.Screen name="camera" options={{ title: "Camera" }} /> */}
+                <Stack.Screen
+                    name="processed-text"
+                    options={{ title: "Processed Text" }}
+                />
+            </Stack>
+        </ImageProvider>
     );
 }
